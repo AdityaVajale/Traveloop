d0c9e8dd6c9893cc0124301b4b5e4daff8e4642c
adityavajale <129027673+adityavajale@users.noreply.github.com>
2025-06-26 11:58:13 +0530

    initial

--- src/main/java/entities/form/RegisterServlet.java
+++ src/main/java/entities/form/RegisterServlet.java
@@ -5,13 +5,17 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.tomcat.dbcp.dbcp2.cpdsadapter.PStmtKeyCPDS;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 @WebServlet("/submit_registration")
 public class RegisterServlet extends HttpServlet {
 
